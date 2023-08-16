1e8f5efb7c11d81d345596bf5eb9f6c790fbcc82
damaka <jaloka@web.de>
2023-08-16 13:06:17 +0200

    dawi sakwa

--- git1/Hello.java
+++ git1/Hello.java
@@ -4,7 +4,7 @@ public static void main(String[]args){
 
 	System.out.println("Hello Git");
 	System.out.println("Hello welt");
-	Gruessen gr1 = new Gruessen("Riad");
+	Gruessen gr1 = new Gruessen("Brahim","Riad",51;);
 	System.out.println(gr1.gruesse());
 
 	
